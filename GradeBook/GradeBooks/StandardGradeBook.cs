b9f8e7ec076ae3c111197b7708710bea0f1c6615
Yvett <yvett.szilagyi@method4.co.uk>
2020-09-23 15:43:07 +0100

    minor

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,6 +9,7 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name) : base(name)
         {
+            Type = GradeBookType.Standard;
         }
     }
 }
