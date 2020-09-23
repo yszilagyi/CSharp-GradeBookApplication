6968bb316cea715d30471cd94e6499a83698c32a
Yvett <yvett.szilagyi@method4.co.uk>
2020-09-23 15:58:05 +0100

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,7 +12,31 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+        public override void CalculateStatistics()
+        {
+            if (this.Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 " +
+                    "students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (this.Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 " +
+                    "students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
 
+        }
         public override char GetLetterGrade(double averageGrade)
         {
             if (this.Students.Count < 5)
